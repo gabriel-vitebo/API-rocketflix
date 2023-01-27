035418c799096cc090c09b570c275ba6c14b42eb
Gabriel Vitebo <vitebo@outlook.com>
2023-01-27 19:51:31 -0300

    creating the route to register the movieNotes

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -2,6 +2,26 @@ const knex = require("../database/knex")
 
 class MovieNotesController {
   async create(request, response) {
+    const { title, description, rating, tags} = request.body
+    const { user_id } = request.params
+
+    const note_id = await knex("movieNotes").insert({
+      title,
+      description,
+      rating
+    })
+
+    const tagsInsert = tags.map( name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex("tags").insert(tagsInsert)
+
+    response.json()
 
   }
 }
