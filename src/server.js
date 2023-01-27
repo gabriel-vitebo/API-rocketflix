d3133454b7401f0f8bb9ebca53f938b9d57c848f
Gabriel Vitebo <vitebo@outlook.com>
2023-01-27 15:35:11 -0300

    creating the database with migrations

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require("express-async-errors")
 
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
-const express = require('express')
+const express = require("express")
 
 const routes = require("./routes")
+migrationsRun()
 
 const app = express()
 app.use(express.json())
@@ -11,20 +13,19 @@ app.use(express.json())
 app.use(routes)
 
 app.use((error, request, response, next) => {
-  if(error instanceof AppError) {
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
-      message: error.message
+      message: error.message,
     })
   }
-  
+
   console.error(error)
 
   return response.status(500).json({
     status: "error",
-    message: error.message
+    message: error.message,
   })
-
 })
 
 const PORT = 3333
