d231f3e00de852fa1a4f8ef9f7676f1e4e812161
Gabriel Vitebo <vitebo@outlook.com>
2023-01-27 15:07:47 -0300

    handling exceptions

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UsersController{
   async create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name) {
+      throw new AppError("O nome é obrigatório!")
+    }
 
     response.json({ name, email, password })
   }
