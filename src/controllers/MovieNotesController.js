c3cc4dddae49dc9fcabdbc1f426ca80892500e28
Gabriel Vitebo <vitebo@outlook.com>
2023-01-29 14:39:09 -0300

    filtering the tags

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -53,13 +53,41 @@ class MovieNotesController {
   }
 
   async index(request, response) {
-    const { title, user_id } = request.query
-    const notes = await knex("movieNotes")
-    .where({ user_id })
-    .whereLike("title", `%${title}%`)
-    .orderBy("title")
+    const { title, user_id, tags } = request.query
 
-    return response.json(notes)
+    let notes
+
+    if(tags) {
+      const filterTags = tags.split(',').map( tag => tag.trim())
+
+      notes = await knex("tags")
+        .select(["movieNotes.id", "movieNotes.title", "movieNotes.user_id"])
+        .where("movieNotes.user_id", user_id)
+        .whereLike("movieNotes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("movieNotes", "movieNotes.id", "tags.note_id")
+        .orderBy("movieNotes.title")
+
+    } else{
+      notes = await knex("movieNotes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title")
+    }
+
+    const userTags = await knex("tags").where({ user_id })
+    const notesWithTags = notes.map( note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+      
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+    
+    
+    return response.json(notesWithTags)
+    
   }
 }
 
