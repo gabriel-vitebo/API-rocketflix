35aaff3ea69d0142054123862823095727ec6061
Gabriel Vitebo <vitebo@outlook.com>
2023-01-29 15:18:10 -0300

    listing the tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 
 const usersRoutes = require("./users.routes")
 const movieNotesRoutes = require("./movieNotes.routes")
+const tagsRouter = require("./tags.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRoutes)
 routes.use("/movienotes", movieNotesRoutes)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
